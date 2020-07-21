6f04b047097ae35d1be0a08a3d607d13a88a6d9f
Yuivae <gynserhan@gmail.com>
2020-07-21 23:53:14 +0200

    removeHandler needed

--- src/Components/EnterItem.js
+++ src/Components/EnterItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function EnterItem() {
+function EnterItem({ itemNo, totalCount, handleRemove }) {
   return (
     <div className="item-content">
       <div className="item-container">
@@ -8,9 +8,12 @@ function EnterItem() {
           <button className="add-button"></button>
           <div className="add-text">
             <h2>Add file</h2>
-            <p>or select a folder</p>
+            <p style={{ cursor: "pointer" }}>or select a folder</p>
           </div>
-          <div className="item-counter"></div>
+          <div className="item-counter">
+            {itemNo}/{totalCount}
+          </div>
+          <div className="remove-item" onClick={handleRemove}></div>
         </div>
         <div
           className="description"
--- src/Components/Mobile.js
+++ src/Components/Mobile.js
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import EnterItem from "./EnterItem";
 
 function Mobile() {
   //setting a dynamic case state
   const [caseID, setCaseID] = useState("#1924");
+  const [itemCount, setItemCount] = useState(1);
+  const [itemList, setItemList] = useState([0]);
+  function addField() {
+    setItemList([...itemList, itemList.length]);
+    console.log(itemList);
+  }
+  function removeHandler() {}
   return (
     <div id="mobile">
       <div id="header">
@@ -12,10 +19,20 @@ function Mobile() {
         </div>
       </div>
       <div id="middle">
-        <EnterItem />
+        {itemList.map((item, index) => (
+          <EnterItem
+            key={index}
+            itemNo={index + 1}
+            totalCount={itemList.length}
+            handleRemove={removeHandler}
+          />
+        ))}
       </div>
       <div id="footer">
-        <div id="submit-section"></div>
+        <button id="add-field" onClick={addField}>
+          Add Field
+        </button>
+        <button id="submit">Submit</button>
       </div>
     </div>
   );
