bc39eeee44885973c264363fec0014b88fbf465b
Yuivae <gynserhan@gmail.com>
2020-07-22 11:47:48 +0200

    Updates

---
+++ precache-manifest.bd238148afde94143c27f0f95c31b71a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6d63b845faa42497c49bdfce769114",
+    "url": "/parsend/index.html"
+  },
+  {
+    "revision": "84ed01d6f74d197f394f",
+    "url": "/parsend/static/css/main.c7d9db0a.chunk.css"
+  },
+  {
+    "revision": "9bb9f6c79721e9ceb57a",
+    "url": "/parsend/static/js/2.6bd35891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parsend/static/js/2.6bd35891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ed01d6f74d197f394f",
+    "url": "/parsend/static/js/main.4adf8522.chunk.js"
+  },
+  {
+    "revision": "cc2c6c8df85ca4507aa4",
+    "url": "/parsend/static/js/runtime-main.867bb8ba.js"
+  }
+]);
